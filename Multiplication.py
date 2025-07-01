fb83f12c958422cd8d5370fea07999bf037220f3
sneha-purohit <snehapurohit04@gmail.com>
2025-07-01 16:48:16 +0530

    Delete Multiplication.py

--- Multiplication.py
+++
@@ -1,7 +0,0 @@
-# https://www.geeksforgeeks.org/program-to-print-multiplication-table-of-a-number/
-
-def printTable(n):
-    for i in range (1, 11): 
-        print ("%d * %d = %d" % (n, i, n * i))
-
-printTable(8)
