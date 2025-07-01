2f14d016b77f281aaa8ff2b4bb07ac6bd7e714d0
sneha-purohit <snehapurohit04@gmail.com>
2025-07-01 16:49:02 +0530

    Delete Sum of square- first n natural number.py

--- Sum of square- first n natural number.py
+++
@@ -1,8 +0,0 @@
-# https://www.geeksforgeeks.org/sum-of-squares-of-first-n-natural-numbers/
-
-def summation(n):
-    return sum([i**2 for i in 
-               range(1, n + 1)])
- 
- 
-print(summation(10)) 
