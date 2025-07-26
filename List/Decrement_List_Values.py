0b5dad7ed50e3d890ff7c0806b5fcde7cf1407ea
sneha-purohit <snehapurohit04@gmail.com>
2025-07-26 23:05:24 +0530

    Add files via upload

---
+++ List/Decrement_List_Values.py
@@ -0,0 +1,9 @@
+# https://www.geeksforgeeks.org//problems/decrement-list-values/1&selectedLang=python3
+
+
+arr = [54, 43, 2, 1, 5] 
+i = 0
+for val in arr:
+    a = arr[i] - 1
+    print(a,end= " ")
+    i += 1
