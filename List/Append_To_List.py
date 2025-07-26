ad85006d0000e6e87a32f2e6789dd64eb9e9ff2e
sneha-purohit <snehapurohit04@gmail.com>
2025-07-26 23:04:55 +0530

    Add files via upload

---
+++ List/Append_To_List.py
@@ -0,0 +1,11 @@
+# https://www.geeksforgeeks.org//problems/append-to-list/1&selectedLang=python3
+
+a = 1
+b = 2 
+c = 3
+
+l =[]
+l.append(a)
+l.append(b)
+l.append(c)
+print(*l)   # Print the list elements separated by space
